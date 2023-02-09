c67756a2698c08717b38f8c56319317386032d46
Dev-Bittu <devbittu@proton.me>
2023-02-09 16:54:42 +0530

    Fixed bug in saving ip address in account_user table by GenericIPAddressField

--- account/models.py
+++ account/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
     email = models.EmailField(_("email address"), blank=True)
-    ip = models.IPAddressField()
+    ip = models.GenericIPAddressField(null=True, blank=True)
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
